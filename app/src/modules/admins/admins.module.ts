6673056a865fc3bc5d8e651983c519f1afbcd54d
Victoria Dzyuba <47743417+mohorka@users.noreply.github.com>
2021-03-02 19:56:37 +0300

    fix admin model

--- app/src/modules/admins/admins.module.ts
+++ app/src/modules/admins/admins.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Admin, AdminSchema } from './schemas/admins.schema';
 import { AdminsService } from './services/admins.service';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }]),
+    ],
     providers: [AdminsService],
     exports: [AdminsService],
 })
